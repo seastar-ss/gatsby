46665611aefd7163fc622f4cca40d4b80d9e8a05
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-01-24 23:01:35 +0100

    test(artifacts): check if only subset of pages gets recreated and files get deleted

--- integration-tests/artifacts/__tests__/index.js
+++ integration-tests/artifacts/__tests__/index.js
@@ -3,13 +3,55 @@ const path = require(`path`)
 const { murmurhash } = require(`babel-plugin-remove-graphql-queries`)
 const { readPageData } = require(`gatsby/dist/utils/page-data`)
 const { stripIgnoredCharacters } = require(`gatsby/graphql`)
-const fs = require(`fs`)
+const fs = require(`fs-extra`)
 
 jest.setTimeout(100000)
 
 const publicDir = path.join(process.cwd(), `public`)
 
 const gatsbyBin = path.join(`node_modules`, `.bin`, `gatsby`)
+
+const manifest = {}
+
+function runGatsbyWithRunTestSetup(runNumber = 1) {
+  return function beforeAllImpl() {
+    return new Promise(resolve => {
+      const gatsbyProcess = spawn(gatsbyBin, [`build`, `--write-to-file`], {
+        stdio: [`inherit`, `inherit`, `inherit`, `inherit`],
+        env: {
+          ...process.env,
+          NODE_ENV: `production`,
+          GATSBY_EXPERIMENTAL_PAGE_BUILD_ON_DATA_CHANGES: `1`, // temporary - will remove when mode is made default,
+          ARTIFACTS_RUN_SETUP: runNumber.toString(),
+        },
+      })
+
+      gatsbyProcess.on(`exit`, () => {
+        manifest[runNumber] = {
+          generated: fs
+            .readFileSync(
+              path.join(process.cwd(), `.cache`, `newPages.txt`),
+              `utf-8`
+            )
+            .split(`\n`)
+            .filter(Boolean),
+          removed: fs
+            .readFileSync(
+              path.join(process.cwd(), `.cache`, `deletedPages.txt`),
+              `utf-8`
+            )
+            .split(`\n`)
+            .filter(Boolean),
+          allPages: fs.readJSONSync(
+            path.join(process.cwd(), `.cache`, `test-pages.json`)
+          ),
+        }
+
+        resolve()
+      })
+    })
+  }
+}
 
 const titleQuery = `
   {
@@ -90,7 +132,7 @@ function assertFileExistenceForPagePaths({ pagePaths, type, shouldExist }) {
   )
 }
 
-beforeAll(async done => {
+beforeAll(done => {
   const gatsbyCleanProcess = spawn(gatsbyBin, [`clean`], {
     stdio: [`inherit`, `inherit`, `inherit`, `inherit`],
     env: {
@@ -99,26 +141,13 @@ beforeAll(async done => {
     },
   })
 
-  gatsbyCleanProcess.on(`exit`, exitCode => {
+  gatsbyCleanProcess.on(`exit`, () => {
     done()
   })
 })
 
-describe(`First run`, () => {
-  beforeAll(async done => {
-    const gatsbyProcess = spawn(gatsbyBin, [`build`], {
-      stdio: [`inherit`, `inherit`, `inherit`, `inherit`],
-      env: {
-        ...process.env,
-        NODE_ENV: `production`,
-        RUN_FOR_STALE_PAGE_ARTIFICATS: `1`,
-      },
-    })
-
-    gatsbyProcess.on(`exit`, exitCode => {
-      done()
-    })
-  })
+describe(`First run (baseline)`, () => {
+  beforeAll(runGatsbyWithRunTestSetup(1))
 
   describe(`Static Queries`, () => {
     test(`are written correctly when inline`, async () => {
@@ -211,7 +240,7 @@ describe(`First run`, () => {
       expect(staticQueryHashes.sort()).toEqual(queries.map(hashQuery).sort())
     })
 
-    test("are written correctly with circular dependency", async () => {
+    test(`are written correctly with circular dependency`, async () => {
       const queries = [titleQuery, ...globalQueries]
       const pagePath = `/circular-dep/`
 
@@ -231,7 +260,7 @@ describe(`First run`, () => {
   })
 
   const expectedPages = [`stale-pages/stable`, `stale-pages/only-in-first`]
-  const unexpectedPages = [`stale-pages/only-in-second`]
+  const unexpectedPages = [`stale-pages/only-not-in-first`]
 
   describe(`html files`, () => {
     const type = `html`
@@ -250,6 +279,10 @@ describe(`First run`, () => {
         type,
         shouldExist: false,
       })
+    })
+
+    it(`should create all html files`, () => {
+      expect(manifest[1].generated.sort()).toEqual(manifest[1].allPages.sort())
     })
   })
 
@@ -274,23 +307,116 @@ describe(`First run`, () => {
   })
 })
 
-describe(`Second run`, () => {
-  const expectedPages = [`stale-pages/stable`, `stale-pages/only-in-second`]
-  const unexpectedPages = [`stale-pages/only-in-first`]
+describe(`Second run (different pages created, data changed)`, () => {
+  const expectedPagesToBeGenerated = [
+    `/stale-pages/only-not-in-first`,
+    `/page-query-changing-data-but-not-id/`,
+    `/page-query-dynamic-2/`,
+  ]
 
-  beforeAll(async done => {
-    const gatsbyProcess = spawn(gatsbyBin, [`build`], {
-      stdio: [`inherit`, `inherit`, `inherit`, `inherit`],
-      env: {
-        ...process.env,
-        NODE_ENV: `production`,
-        RUN_FOR_STALE_PAGE_ARTIFICATS: `2`,
-      },
+  const expectedPagesToRemainFromPreviousBuild = [
+    `/stale-pages/stable/`,
+    `/page-query-stable/`,
+    `/page-query-changing-but-not-invalidating-html/`,
+  ]
+
+  const expectedPages = [
+    // this page should remain from first build
+    ...expectedPagesToRemainFromPreviousBuild,
+    // those pages should have been (re)created
+    ...expectedPagesToBeGenerated,
+  ]
+
+  const unexpectedPages = [
+    `/stale-pages/only-in-first/`,
+    `/page-query-dynamic-1/`,
+  ]
+
+  beforeAll(runGatsbyWithRunTestSetup(2))
+
+  describe(`html files`, () => {
+    const type = `html`
+
+    describe(`should have expected html files`, () => {
+      assertFileExistenceForPagePaths({
+        pagePaths: expectedPages,
+        type,
+        shouldExist: true,
+      })
     })
 
-    gatsbyProcess.on(`exit`, exitCode => {
-      done()
+    describe(`shouldn't have unexpected html files`, () => {
+      assertFileExistenceForPagePaths({
+        pagePaths: unexpectedPages,
+        type,
+        shouldExist: false,
+      })
     })
+
+    it(`should recreate only some html files`, () => {
+      expect(manifest[2].generated.sort()).toEqual(
+        expectedPagesToBeGenerated.sort()
+      )
+    })
+  })
+
+  describe(`page-data files`, () => {
+    const type = `page-data`
+
+    describe(`should have expected page-data files`, () => {
+      assertFileExistenceForPagePaths({
+        pagePaths: expectedPages,
+        type,
+        shouldExist: true,
+      })
+    })
+
+    describe(`shouldn't have unexpected page-data files`, () => {
+      assertFileExistenceForPagePaths({
+        pagePaths: unexpectedPages,
+        type,
+        shouldExist: false,
+      })
+    })
+  })
+})
+
+describe(`Third run (js change, all pages are recreated)`, () => {
+  const expectedPages = [
+    `/stale-pages/only-not-in-first`,
+    `/page-query-dynamic-3/`,
+  ]
+
+  const unexpectedPages = [
+    `/stale-pages/only-in-first/`,
+    `/page-query-dynamic-1/`,
+    `/page-query-dynamic-2/`,
+  ]
+
+  let changedFileOriginalContent
+  const changedFileAbspath = path.join(
+    process.cwd(),
+    `src`,
+    `pages`,
+    `gatsby-browser.js`
+  )
+
+  beforeAll(async () => {
+    // make change to some .js
+    changedFileOriginalContent = fs.readFileSync(changedFileAbspath, `utf-8`)
+
+    const newContent = changedFileOriginalContent.replace(/sad/g, `not happy`)
+
+    if (newContent === changedFileOriginalContent) {
+      throw new Error(`Test setup failed`)
+    }
+
+    fs.writeFileSync(changedFileAbspath, newContent)
+    await runGatsbyWithRunTestSetup(3)()
+  })
+
+  afterAll(() => {
+    fs.writeFileSync(changedFileAbspath, changedFileOriginalContent)
   })
 
   describe(`html files`, () => {
@@ -310,6 +436,10 @@ describe(`Second run`, () => {
         type,
         shouldExist: false,
       })
+    })
+
+    it(`should recreate all html files`, () => {
+      expect(manifest[3].generated.sort()).toEqual(manifest[3].allPages.sort())
     })
   })
 
--- integration-tests/artifacts/gatsby-node.js
+++ integration-tests/artifacts/gatsby-node.js
@@ -1,6 +1,50 @@
-const isFirstRun = process.env.RUN_FOR_STALE_PAGE_ARTIFICATS !== `2`
+const path = require(`path`)
+const fs = require(`fs-extra`)
+
+const runNumber = parseInt(process.env.ARTIFACTS_RUN_SETUP, 10) || 1
+
+const isFirstRun = runNumber === 1
+
+exports.onPreInit = ({ reporter }) => {
+  reporter.info(`Using test setup #${runNumber}`)
+}
+
+exports.sourceNodes = ({ actions, createContentDigest }) => {
+  // used to create pages and queried by them
+  function createNodeHelper(type, nodePartial) {
+    actions.createNode({
+      ...nodePartial,
+      internal: {
+        type,
+        contentDigest: createContentDigest(nodePartial),
+      },
+    })
+  }
+
+  createNodeHelper(`DepPageQuery`, {
+    id: `page-query-stable`,
+    label: `Stable (always created)`,
+  })
+
+  createNodeHelper(`DepPageQuery`, {
+    id: `page-query-changing-but-not-invalidating-html`,
+    label: `Stable (always created)`,
+    buildRun: runNumber, // important for test setup - this will invalidate page queries, but shouldn't invalidate html (if it's not queried)
+  })
+
+  createNodeHelper(`DepPageQuery`, {
+    id: `page-query-changing-data-but-not-id`,
+    label: `This is${isFirstRun ? `` : ` not`} a first run`, // this will be queried - we want to invalidate html here
+  })
+
+  createNodeHelper(`DepPageQuery`, {
+    id: `page-query-dynamic-${runNumber}`, // this should cause different page path
+    label: `This is run number {$runNumber}`,
+  })
+}
 
 exports.createPages = ({ actions }) => {
+  // testing if expected html/page-data files exist OR don't exist (if stale artifacts are removed)
   function createPageHelper(dummyId) {
     actions.createPage({
       path: `/stale-pages/${dummyId}`,
@@ -15,10 +59,27 @@ exports.createPages = ({ actions }) => {
   createPageHelper(`stable`)
 
   if (isFirstRun) {
-    // page exists in first run, but not in second
+    // page exists only in first run
     createPageHelper(`only-in-first`)
   } else {
-    // page exists only in second run
-    createPageHelper(`only-in-second`)
+    // page exists in any run other than first
+    createPageHelper(`only-not-in-first`)
   }
+}
+
+exports.onPostBuild = async ({ graphql }) => {
+  const { data } = await graphql(`
+    {
+      allSitePage(filter: { path: { ne: "/dev-404-page/" } }) {
+        nodes {
+          path
+        }
+      }
+    }
+  `)
+
+  fs.writeJSONSync(
+    path.join(process.cwd(), `.cache`, `test-pages.json`),
+    data.allSitePage.nodes.map(node => node.path)
+  )
 }
